fcf7989a1089f5d00b9b30743941272d8411cdbb
Viktor Hahn <viktor.hahn@web.de>
2021-10-22 23:44:19 +0200

    Added new hair styles.

--- scripts/hair.gd
+++ scripts/hair.gd
@@ -1,8 +1,8 @@
 tool
 extends Sprite
 
-enum Sprites {NONE,FEMALE01,FEMALE02,FEMALE03,FEMALE04,FEMALE06,FEMALE07,FEMALE08,FEMALE09,\
-PONYTAIL01,MALE01}
+enum Sprites {NONE,FEMALE01,FEMALE02,FEMALE03,FEMALE04,FEMALE06,FEMALE07,FEMALE08,FEMALE09,FEMALE10,\
+PONYTAIL01,MALE01,MALE02,MALE03}
 const Male = Sprites.MALE01
 const Female = Sprites.FEMALE01
 const FILES = {
@@ -14,9 +14,11 @@ const FILES = {
 	Sprites.FEMALE07:"res://images/hair/base/female07.png",
 	Sprites.FEMALE08:"res://images/hair/base/female08.png",
 	Sprites.FEMALE09:"res://images/hair/base/female09.png",
+	Sprites.FEMALE10:"res://images/hair/base/female10.png",
 	Sprites.PONYTAIL01:"res://images/hair/base/ponytail01.png",
 	Sprites.MALE01:"res://images/hair/base/male01.png",
-#	Sprites.MALE02:"res://images/hair/base/male02.png",
+	Sprites.MALE02:"res://images/hair/base/male02.png",
+	Sprites.MALE03:"res://images/hair/base/male03.png",
 }
 
 export(Sprites) var sprite:= Sprites.FEMALE01 setget set_sprite
